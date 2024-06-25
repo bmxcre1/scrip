5e54d012c91129ecad34176560f32561c7274fc9
bmxcre1 <78355953+bmxcre1@users.noreply.github.com>
2024-06-25 18:47:58 +0100

    Update GetRawMetatable.lua

--- GetRawMetatable.lua
+++ GetRawMetatable.lua
@@ -1,35 +1,70 @@
-local function deep_copy(orig)
-    local orig_type = type(orig)
-    local copy
-    if orig_type == 'table' then
-        copy = {}
-        for orig_key, orig_value in next, orig, nil do
-            copy[deep_copy(orig_key)] = deep_copy(orig_value)
-        end
-        setmetatable(copy, deep_copy(getmetatable(orig)))
-    else
-        copy = orig
-    end
-    return copy
-end
-
 getgenv().getrawmetatable = function(obj)
-    local raw_meta = debug.getmetatable(obj)
-    if not raw_meta then return nil end
+    local metatable = getmetatable(obj)
+    if not metatable then return nil end
 
-    local function extract_tables(tbl)
-        local tables = {}
-        for k, v in pairs(tbl) do
-            if type(v) == "table" then
-                tables[k] = deep_copy(v)
-            end
-        end
-        return tables
+    local metatables = {}
+    while metatable do
+        table.insert(metatables, metatable)
+        metatable = getmetatable(metatable)
     end
-
-    local all_tables = extract_tables(raw_meta)
-    return {raw_meta = raw_meta, all_tables = all_tables}
+    return metatables
 end
 
-print("[+] Loaded getrawmetatable(<Object>)")
+function setrawmetatable = function(obj, mt)
+    if type(obj) == 'table' and type(mt) == 'table' then
+        setmetatable(obj, mt)
+        return true
+    else
+        return false, "both arguments must be tables."
+    end
+end
 
+ getgenv().setreadonly(obj, val)
+    if type(obj) == 'table' then
+        local mt = getmetatable(obj) or {}
+        if val then
+            mt.__newindex = function(t, k, v)
+                error("attempt to modify readonly table", 2)
+            end
+        else
+            mt.__newindex = nil
+        end
+        setmetatable(obj, mt)
+    else
+        error("expected a table for the first argument", 2)
+    end
+end
+
+ getgenv().isreadonly = function(obj)
+    if type(obj) == 'table' then
+        local mt = getmetatable(obj)
+        if mt and mt.__newindex then
+            return false
+        end
+        return true
+    else
+        error("expected a table for the argument", 2)
+    end
+end
+
+getgenv().make_readonly = function(obj)
+    if type(obj) == 'table' then
+        local mt = getmetatable(obj) or {}
+        mt.__newindex = function(t, k, v)
+            error("attempt to modify readonly table", 2)
+        end
+        setmetatable(obj, mt)
+    else
+        error("expected a table for the first argument", 2)
+    end
+end
+
+getgenv().make_writeable = function(obj)
+    if type(obj) == 'table' then
+        local mt = getmetatable(obj) or {}
+        mt.__newindex = nil
+        setmetatable(obj, mt)
+    else
+        error("expected a table for the first argument", 2)
+    end
+end
