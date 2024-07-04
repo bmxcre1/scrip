1cfbb6c33933bf26901cb72e5dfbbf6bc4db684c
bmxcre1 <78355953+bmxcre1@users.noreply.github.com>
2024-07-04 20:11:10 +0100

    Update ProximityPrompt.lua

--- ProximityPrompt.lua
+++ ProximityPrompt.lua
@@ -10,20 +10,11 @@ getgenv().fireproximityprompt = function(prompt)
             }
             prompt.RequiresLineOfSight = false 
             prompt.MaxActivationDistance = 999999999
-            prompt.HoldDuration = 0
+            prompt.HoldDuration = 0.05
             prompt.Enabled = true
-            coroutine.wrap(function()
-                for i =1,50 do 
-                    for _, prompt_u in ipairs(game.Players.LocalPlayer.PlayerGui.ProximityPrompts:GetChildren()) do 
-                        if prompt_u.Name == "Prompt"  then
-                            prompt_u.Enabled = false 
-                            prompt_u.Enabled = true
-                        end
-                    end
-                end
-            end)()
-            task.wait(.05)
+            task.wait()
             prompt:InputHoldBegin()
+	    task.wait(.05)
             prompt:InputHoldEnd()
             coroutine.wrap(function()
                 task.wait(.01)
