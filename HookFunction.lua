8cfa75260b6ab09922bd6c375dcb27ef9edfc6c3
bmxcre1 <78355953+bmxcre1@users.noreply.github.com>
2024-07-04 20:01:04 +0100

    Create HookFunction.lua

---
+++ HookFunction.lua
@@ -0,0 +1,10 @@
+getgenv().hookfunction(old_func, hook_func)
+    if type(old_func) == "function" and type(hook_func) == "function" then
+        local original_func = old_func  
+        _G[old_func] = function(...)
+            return hook_func(original_func, ...)
+        end
+        return original_func
+    else
+        return nil, "Both args must be functions"
+    end
